a9afa3a8f30d601b6de843f0ee92990cd3c90193
MdMuzahidKhan <114174484+MdMuzahidKhan@users.noreply.github.com>
2023-03-27 09:24:29 +0000

    added os and crypto files

---
+++ crypto/extendedeuclidean.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+
+int extendedEuclidean(int a, int b, int &x, int &y) {
+    if (b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    int x1, y1;
+    int gcd = extendedEuclidean(b, a % b, x1, y1);
+    x = y1;
+    y = x1 - (a / b) * y1;
+    return gcd;
+}  
+
+
+int main() {
+    int a, b, x, y;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    int gcd = extendedEuclidean(a, b, x, y);
+    cout << "GCD(" << a << ", " << b << ") = " << gcd << endl;
+    cout << "x = " << x << ", y = " << y << endl;
+    return 0;
+}
